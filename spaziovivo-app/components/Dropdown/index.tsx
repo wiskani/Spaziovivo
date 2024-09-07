fc3b54158ffb9609ea253f950896361d18e62c31
wiskani <wiskani@icloud.com>
2024-09-07 09:15:00 -0400

    down component

---
+++ spaziovivo-app/components/Dropdown/index.tsx
@@ -0,0 +1,40 @@
+import {useClickOutside} from '@/hooks/useClickOutside';
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
+
+interface DropdownProps {
+    isOpen: boolean;
+    handleChange?: (i: boolean) => void;
+    children?: React.ReactNode;
+    classes?: string;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+    isOpen,
+    handleChange,
+    children,
+    classes,
+}) => {
+    const dropRef = useRef(null);
+
+    useClickOutside(dropRef, () => {
+        if (isOpen && handleChange) handleChange(false);
+    });
+
+    return (
+        <motion.div
+            initial={{ y: -500 }}
+            animate={{ y: 0 }}
+            className={`${classes} 
+            absolute flex-col right-0 w-96 p-4 mt-2 rounded-md shadow-lg z-30 bg-white drop__wrapper wrapper__white animate-fade-in-down
+            ${isOpen ? "flex" : "hidden"}
+
+`}
+            ref={dropRef}
+        >
+                {children}
+            </motion.div>
+    );
+};
+
+export default Dropdown;
