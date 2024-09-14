c043f774478d3f37adc0a642caf430aee21f3597
wiskani <wiskani@icloud.com>
2024-09-14 06:57:00 -0400

    adding filters hero component

--- spaziovivo-app/stories/FDropdownLocation.stories.ts
+++ spaziovivo-app/stories/FDropdownLocation.stories.ts
@@ -1,2 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import FDropdownLocation from '@/components/Filters/FDropdownLocation';
+
+const meta: Meta<typeof FDropdownLocation> = {
+    component: FDropdownLocation,
+}
+
+export default meta;
+type Story = StoryObj<typeof FDropdownLocation>
+
+export const Primary: Story = {
+    args: {
+
+    }
+}
