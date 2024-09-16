d3998d06f6cbc28ad46df465d589e255baf7162c
wiskani <wiskani@icloud.com>
2024-09-15 22:40:13 -0400

    Footer and navbar

---
+++ spaziovivo-app/components/Fotter/index.tsx
@@ -0,0 +1,71 @@
+import Container from "@/containers/home-page/container";
+import Link from "next/link";
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = ({}) => {
+    return (
+        <footer className="mt-12 bg-black222 border-t-2 border-red-500">
+        <Container classes="px-3">
+            <div className="flex flex-col md:flex-row py-16">
+                <div className="flex-1.5 flex flex-col items-center md:items-start">
+                    <Link href="/">
+                        <a>
+                            <h1 className="font-charm text-primary text-4xl md:text-6xl font-bold">
+                               Spaziovivo 
+                            </h1>
+                        </a>
+                    </Link>
+                    <p className="mt-2 text-base text-gray-400 md:w-4/5 text-center md:text-left">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut.
+                    </p>
+
+                </div>
+                <div className="flex-1.5 my-5 md:my-0">
+                    <h5 className="mt-3 text-lg text-center md:text-left mb-3 underline text-white">
+                       Provicincias 
+                    </h5>
+                    <ul className="p-0 m-0 flex flex-col text-center md:text-left">
+                        <li className="mt-1 group">
+                            <Link href="/listings?country=Italy">
+                                <a className="text-base text-desc group-hover:underline text-white">
+                                    Cochabamba
+                                </a>
+                            </Link>
+                        </li>
+                        <li className="mt-1 group">
+                            <Link href="/listings?country=Spain">
+                                <a className="text-base text-desc group-hover:underline text-white">
+                                    Santa Cruz
+                                </a>
+                            </Link>
+                        </li>
+                        <li className="mt-1 group">
+                            <Link href="/listings?country=United Kingdom">
+                                <a className="text-base text-desc group-hover:underline text-white">
+                                    La Paz
+                                </a>
+                            </Link>
+                        </li>
+                        <li className="mt-1 group">
+                            <Link href="/listings?country=Portugal">
+                                <a className="text-base text-desc group-hover:underline text-white">
+                                    Oruro
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </Container>
+        <div className="flex flex-col md:flex-row bg-black222 justify-center items-center py-3">
+            <p className="text-base text-white md:mx-3 mb-2 md:mb-0">
+                Made by wiskani 
+            </p>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
--- spaziovivo-app/components/Navbar/index.tsx
+++ spaziovivo-app/components/Navbar/index.tsx
@@ -3,3 +3,75 @@ import { Slant as Hamburger } from "hamburger-react";
 import Container from "@/containers/home-page/container";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import FullMenu from "../FullMenu";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps>= ({}) => {
+    const router = useRouter();
+    const [isOpen, setOpen] = useState(false);
+    const handleToggle = () => setOpen(!isOpen);
+
+    return(
+        <>
+        <header
+            className={`
+                z-20 ${
+                    router.pathname! == "/"
+                    ? "relative border-b-2 border-light"
+                    : "absolute left-0 right-0 top-8"
+                }
+                `}
+        >
+            <Container classes="px-7">
+            <div className="flex items-center justify-between">
+                        <div className="flex-none">
+                            <Link href="/">
+                                <a>
+                                    <h1 className="font-charm text-primary text-4xl font-bold">
+                                        FT
+                                    </h1>
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="flex-none hidden md:block">
+                            <nav>
+                                <ul className="flex justify-items-end items-center">
+                                    <li className="mx-4 relative">
+                                        <Link href="/">
+                                            <a className="text-white text-base custom__hover--nav block py-6">
+                                                Home
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li className="mx-4 relative">
+                                        <Link href="/listings">
+                                            <a className="text-white text-base custom__hover--nav block py-6">
+                                                Lista de propiedades
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li className="mx-4">
+                                        <Hamburger
+                                            toggled={isOpen}
+                                            toggle={handleToggle}
+                                            color="#fff"
+                                        />
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="flex-none md:hidden py-5">
+                            <Hamburger
+                                toggled={isOpen}
+                                toggle={handleToggle}
+                                color="#fff"
+                            />
+                        </div>
+                    </div>
+            </Container>
+        </header>
+        {isOpen && <FullMenu handleClose={handleToggle} />}
+        </>
+    )
+}
